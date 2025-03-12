9b25da9532a6f8828c36c572d5f55ef6c048c543
MenfisGualip <17009994@galileo.edu>
2025-03-11 22:47:44 -0600

    Agregando cambios en frontend

---
+++ features/habit/habitSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchHabits } from './habitAPI';
+
+type Habit = {
+    _id: string;
+    title: string;
+    description: string;
+}
+
+type HabitState = {
+    habits: Habit[];
+}
+
+const initialState: HabitState = {
+    habits: []
+}
+export const fetchHabitsThunk = createAsyncThunk('habits/fetchHabits', async () => {
+    const response = await fetchHabits();
+    const responseJson = await response.json();
+    return responseJson;
+});
+
+const habitSlice = createSlice({
+    name: 'habits',
+    initialState,
+    reducers: {
+        addHabits: (state, action) => {
+            state.habits = action.payload;
+        }
+    },
+    extraReducers: builder => {
+        builder.addCase(fetchHabitsThunk.fulfilled, (state, action) => {
+            state.habits = action.payload;
+        })
+    }
+})
+
+export const { addHabits } = habitSlice.actions;
+export default habitSlice.reducer;
